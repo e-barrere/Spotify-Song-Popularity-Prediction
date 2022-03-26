e71f6824d45012693af8b6f3296b7ac8bebe5490
e-barrere <56220970+e-barrere@users.noreply.github.com>
2022-03-26 09:13:07 +0100

    Matthieu's model

--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -1,1 +1,30 @@
 import pandas as pd 
+import numpy as np
+ 
+from sklearn.base import BaseEstimator
+
+import pandas as pd
+import numpy as np
+ 
+
+from sklearn import linear_model
+
+class Regressor(BaseEstimator):
+    def __init__(self):
+        
+        self.model = linear_model.LinearRegression()
+
+    def fit(self, X, Y):
+
+        self.X_maxs = np.max(X, axis=0)
+        X = X / self.X_maxs
+        self.model.fit(X, Y)
+ 
+    def predict(self, X):
+
+        # normalization
+        X = X / self.X_maxs
+
+        preds = self.model.predict(X)
+
+        return preds
